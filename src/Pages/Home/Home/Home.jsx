37fa7fab63b9d534284c6e28413d4c815b501f15
EngrMunir <smsirajulmonir@gmail.com>
2024-06-13 23:56:20 -0700

    swiper slider added

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Category from "../../Category/Category";
+import Slider from "../Slider/Slider";
+
+const Home = () => {
+    return (
+        <div>
+            <Slider></Slider>
+            <Category></Category>
+        </div>
+    );
+};
+
+export default Home;
